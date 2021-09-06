c2a66de3af1ad5fd1560f92c0733ca9cff948fee
kazu228 <kskkm0228@gmail.com>
2021-09-06 19:22:40 +0900

    ブロックを描画する前まで

--- tetris.py
+++ tetris.py
@@ -4,6 +4,26 @@ win = Tk()
 cv = Canvas(win, width=400, height=700, background="#000")
 cv.pack()
 
+data = [
+    [1, 0],
+    [1, 1],
+    [0, 1]
+]
+
+def is_empty(data):
+    num = len(data)
+    if num == 0:
+        return True
+    else:
+        return False
+
+def draw(data):
+    if not is_empty(data):
+        #dataがあったとき、テトリスのブロック描画
+        for d in data:
+            for b in d:
+                if b == 1: #dataが1、それ即ち、ブロックが存在する場合
+                    cv.create_rectangle()
 
 
 win.mainloop()
