c4e13a97a235617d619c96070ec2575f196427a2
kazu228 <kskkm0228@gmail.com>
2021-09-08 10:08:00 +0900

    ブロックの要件を追記

--- tetris.py
+++ tetris.py
@@ -10,6 +10,13 @@ data = [
     [0, 1]
 ]
 
+block_start_position_x, block_start_position_y = [200, 0]
+
+block_size = {
+    "x": 10,
+    "y": 10
+}
+
 def is_empty(data):
     num = len(data)
     if num == 0:
@@ -22,7 +29,7 @@ def draw(data):
         #dataがあったとき、テトリスのブロック描画
         for d in data:
             for b in d:
-                if b == 1: #dataが1、それ即ち、ブロックが存在する場合
+                if b == 1: #dataが1、即ち、ブロックが存在する場合
                     cv.create_rectangle()
 
 
