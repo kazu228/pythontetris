6d2bc945d9dc2baec8f417c04c891b1b88f7a626
kazu228 <kskkm0228@gmail.com>
2021-09-09 17:46:23 +0900

    データから描画

--- tetris.py
+++ tetris.py
@@ -30,18 +30,22 @@ def draw(data):
     global block_start_position_x, block_start_position_y
     if not is_empty(data):
         #dataがあったとき、テトリスのブロック描画
+        # cv.create_rectangle(block_start_position_x - block_size["x"], block_start_position_y - block_size["y"],
+        #                                 block_start_position_x + block_size["x"], block_start_position_y + block_size["y"], fill="red")
+        # print(block_start_position_x - block_size["x"])
+        # print(block_start_position_x + block_size["x"])
         for d in data:
             for b in d:
-                print(block_start_position_x)
                 if b == 1: #dataが1、即ち、ブロックが存在する場合
                     # print('aaa')
                     cv.create_rectangle(block_start_position_x - block_size["x"], block_start_position_y - block_size["y"],
                                         block_start_position_x + block_size["x"], block_start_position_y + block_size["y"], fill="red")
 
-                block_start_position_x += block_size["x"]
+                # ブロックの幅分、ずらす。
+                block_start_position_x += block_size["x"] * 2  
 
             block_start_position_x = 200
-            block_start_position_y += block_size["y"]
+            block_start_position_y += block_size["y"] * 2     # ブロックの幅分、ずらす。
 
 def main(data):
     draw(data)
