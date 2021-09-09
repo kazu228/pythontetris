5f7c45d4a9953a8436b83032cffe0acb25835f5b
kazu228 <kskkm0228@gmail.com>
2021-09-09 11:09:06 +0900

    block_start_position_x, yをグローバル変数に

--- tetris.py
+++ tetris.py
@@ -10,7 +10,8 @@ data = [
     [0, 1]
 ]
 
-block_start_position_x, block_start_position_y = [200, 0]
+block_start_position_x = 200
+block_start_position_y = 10
 
 block_size = {
     "x": 10,
@@ -25,12 +26,21 @@ def is_empty(data):
         return False
 
 def draw(data):
+    global block_start_position_x, block_start_position_y
     if not is_empty(data):
         #dataがあったとき、テトリスのブロック描画
         for d in data:
             for b in d:
                 if b == 1: #dataが1、即ち、ブロックが存在する場合
-                    cv.create_rectangle()
+                    cv.create_rectangle(block_start_position_x - block_size["x"], block_start_position_y - block_size["y"],
+                                        block_start_position_x + block_size["x"], block_start_position_y + block_size["y"])
 
+                block_start_position_x += block_size["x"]
 
+            block_start_position_y += block_size["y"]
+
+def main(data):
+    draw(data)
+
+main(data)
 win.mainloop()
